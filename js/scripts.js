1a8187a7eabffdc15f8d48f3a330f5154a7f98d6
Anthony Gold and LaTaevia Berry <lataevia@>
2016-03-22 16:51:21 -0700

    adding buttons, but they dont echo back

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,36 @@
 $(document).ready(function() {
-  // $(".container").load(function() {
-  //   $(".panel-body").hide();
-  // });
-  $(".panel").click(function() {
-    $(".panel-body", this).toggle("hiding");
+
+  $("button#hello").click(function() {
+    $("ul#user").prepend("<li>Hello!</li>");
+    $("ul#webpage").prepend("<li>Why hello there!</li>");
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+    });
+  });
+
+
+  $("button#goodbye").click(function() {
+    $("ul#user").prepend("<li>Goodbye!</li>");
+    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+    });
+  });
+
+  $("button#stop").click(function() {
+    $("ul#user").prepend("<li>Stop copying me!</li>");
+    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+    $("ul#user").children("li").first().click(function() {
+    $(this).remove();
+    });
+    $("ul#webpage").children("li").first().click(function() {
+    $(this).remove();
+    });
   });
 });
-//   $("button#green").click(function() {
-//     $("body").removeClass();
-//     $("body").addClass("green-background");
-//   });
-//
-//   $("button#yellow").click(function() {
-//     $("body").removeClass();
-//     $("body").addClass("yellow-background");
-//   });
-//
-//   $("button#red").click(function() {
-//     $("body").removeClass();
-//     $("body").addClass("red-background");
-//   });
-//   $("button#white").click(function() {
-//     $("body").removeClass();
-//     $("body").addClass("revert-colors");
-//   });
-//   $("button#blue").click(function() {
-//     // $("body").removeClass();
-//     $("#blue-text-border").addClass("add-border");
-//   });
